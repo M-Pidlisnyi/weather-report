28e0b27aa6d9cc9f34c38a30ed361cda0f5b9a6c
M-Pidlisnyi <mishapropatriot@gmail.com>
2022-06-03 12:32:28 +0300

    created city model and connected to api

--- weatherapp/models.py
+++ weatherapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class City(models.Model):
+	name = models.CharField(max_length=250, null=False, blank=False)
+	longitude = models.FloatField()
+	latitude = models.FloatField()
+
+	class Meta:
+		verbose_name_plural="Cities"
--- weatherapp/views.py
+++ weatherapp/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
-
+import requests as pyreq
 # Create your views here.
 
-def index(request):
 
-	return (render(request, 'index.html'))
+def index(request):
+	api_url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid=e0dfe0214875b7cec701cc3e4b5805ec'
+
+	city_name = 'Kalush'
+
+	weather = [pyreq.get(api_url.format(city_name)).json()]
+
+	context = {
+		'city': city_name,
+		'weather': weather[0]
+	}
+
+	return render(request, 'index.html', context=context)
