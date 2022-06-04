18d102856d4e47ca2d207fbb64c29002f57a7603
M-Pidlisnyi <mishapropatriot@gmail.com>
2022-06-04 19:35:59 +0300

    added geocoding

--- weatherapp/views.py
+++ weatherapp/views.py
@@ -10,8 +10,12 @@ from .models import City
 def index(request):
 
 	if request.method == 'POST':
-		# TODO: add check for unique city
-		new_city = City.objects.create(name=request.POST.get('city', 'unknown city'))
+		# TODO: add check if city is unique
+		new_city_name = request.POST.get('city', 'unknown city')
+		geocoding_url = 'https://api.openweathermap.org/geo/1.0/direct?q={}&appid=e291b83f18bced35c5185c59e5920195'
+		api_response = pyreq.get(geocoding_url.format(new_city_name)).json()
+		new_city_coords = (api_response[0]['lat'], api_response[0]['lon'])
+		new_city = City.objects.create(name=new_city_name, latitude=new_city_coords[0], longitude=new_city_coords[1])
 		return HttpResponseRedirect(new_city.get_absolute_url())
 
 	api_url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid=e0dfe0214875b7cec701cc3e4b5805ec'
@@ -33,6 +37,13 @@ class CityDetailView(DetailView):
 		context = super(CityDetailView, self).get_context_data(**kwargs)
 		api_url = 'https://api.openweathermap.org/data/2.5/forecast?q={}&appid=c5665be5e701b810ec09c1dcb5a80f68'
 		context['weather'] = pyreq.get(api_url.format(context['city'].name)).json()
+
+		# TODO: redo line below it's not working properly
+		if not (context['city'].latitude or context['city'].longitude is None):
+			context['coords'] = (context['city'].latitude, context['city'].longitude)
+		else:
+			context['coords'] = None
+
 		return context
 
 	def get_queryset(self):
