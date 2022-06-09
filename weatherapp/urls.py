3f8870e5f0abd1a5309a59009259f1e7c0875650
M-Pidlisnyi <mishapropatriot@gmail.com>
2022-06-09 19:28:36 +0300

    added styles

--- weatherapp/urls.py
+++ weatherapp/urls.py
@@ -1,8 +1,9 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('city/<int:pk>', views.CityDetailView.as_view(), name='city_detail'),
-
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
